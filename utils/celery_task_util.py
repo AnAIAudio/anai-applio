ae71badf96ab97ea6b6331832d11787e823ca391
hanbyulkang <kanghanstar@gmail.com>
2026-01-29 16:57:08 +0900

    Celery 태스크 상태 추적 설정 추가
    작업 상태 및 결과 추적을 위한 설정을 추가했습니다. `CELERY_RESULT_BACKEND` 환경 변수를 통해 Redis를 사용하도록 구성했습니다. main

--- utils/celery_task_util.py
+++ utils/celery_task_util.py
@@ -2,10 +2,12 @@ import os
 from celery import Celery
 
 rabbit_url = os.getenv("RABBIT_URL")
+redis_url = os.getenv("CELERY_RESULT_BACKEND")
 
 celery_app = Celery(
     "worker",
     broker=rabbit_url,
+    backend=redis_url,
     include=[
         "core",
     ],
@@ -18,3 +20,10 @@ celery_app.conf.broker_transport_options = {
 celery_app.conf.timezone = "Asia/Seoul"
 celery_app.conf.worker_prefetch_multiplier = 1
 celery_app.conf.task_acks_late = True
+
+# 상태/결과 관련(Gradio에서 조회할 거라면 권장)
+celery_app.conf.task_track_started = True
+celery_app.conf.result_expires = 10800  # 결과 TTL (필요에 맞게)
+celery_app.conf.accept_content = ["json"]
+celery_app.conf.task_serializer = "json"
+celery_app.conf.result_serializer = "json"
