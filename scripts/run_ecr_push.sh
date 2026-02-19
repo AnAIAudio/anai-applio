b42a07c0fb21155e6192262233be9a1c45a7c11d
hanbyulkang <kanghanstar@gmail.com>
2026-02-19 18:23:05 +0900

    Docker 빌드 플랫폼 추가
    Docker 이미지 빌드 시 `--platform linux/amd64` 설정을 추가해 호환성을 개선했습니다. feature/overtraining_check

--- scripts/run_ecr_push.sh
+++ scripts/run_ecr_push.sh
@@ -4,6 +4,7 @@ aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS
 #docker push 339712720989.dkr.ecr.ap-northeast-1.amazonaws.com/dev/anai-applio:latest
 
 docker buildx build \
+  --platform linux/amd64 \
   --cache-to type=registry,ref=339712720989.dkr.ecr.ap-northeast-1.amazonaws.com/dev/anai-applio:buildcache,mode=max \
   --cache-from type=registry,ref=339712720989.dkr.ecr.ap-northeast-1.amazonaws.com/dev/anai-applio:buildcache \
   --tag 339712720989.dkr.ecr.ap-northeast-1.amazonaws.com/dev/anai-applio:latest \
